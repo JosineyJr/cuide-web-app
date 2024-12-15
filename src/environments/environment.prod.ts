050ebadf6c6f519bd4c4c466fdfeb924de0821d9
JosineyJr <josineyjunior14@gmail.com>
2024-12-15 15:47:51 -0300

    refactor: api prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://39e0-177-74-238-26.ngrok-free.app/v1',
+  apiUrl:
+    'https://88bf-2804-14c-5b87-547b-2937-73c2-5058-b637.ngrok-free.app/v1',
 };
