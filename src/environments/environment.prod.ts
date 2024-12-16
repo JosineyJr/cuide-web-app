d2ee7384f01cf0bccba9322d20453d1346257786
JosineyJr <josineyjunior14@gmail.com>
2024-12-16 11:24:31 -0300

    style: multiselect color

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl:
-    'https://30b9-2804-14c-5b87-547b-2937-73c2-5058-b637.ngrok-free.app/v1',
+    'https://fb28-2804-14c-5b87-547b-2937-73c2-5058-b637.ngrok-free.app/v1',
 };
